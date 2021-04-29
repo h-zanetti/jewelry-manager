8b0760174662f60c5ad0833282c06cb5208635aa
agah <henriaguiar99@gmail.com>
2021-04-29 19:46:22 -0300

    Criados novos arquivos de settings

--- webdev/settings/base.py
+++ webdev/settings/base.py
@@ -12,25 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-# Load environmental variables
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
-
-ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -75,22 +59,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'webdev.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'TEST': {
-            'NAME': 'db_test',
-        }
-    }
-}
 
 
 # Password validation
