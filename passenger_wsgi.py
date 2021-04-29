058fc959d0e53b865f41c21ada0173dde4d9ef23
agah <henriaguiar99@gmail.com>
2021-04-29 15:00:11 -0300

    Consertado passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,2 @@
   
-import sys, os
-
-INTERP = "/home/AgahSolutionsRepositories/jewelry-manager/venv/bin/python"
-
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-
 from webdev.wsgi import application
