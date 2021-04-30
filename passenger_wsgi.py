dfee4927684b06ce07e8b3a8289cb06517fb2e34
agah <henriaguiar99@gmail.com>
2021-04-30 01:20:53 -0300

    Alterado INTERP do passenger_wsgi close #34

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 
-INTERP = "/home/wwagah/AgahSolutionsRepositories/jewelry-manager/venv/bin/python"
+INTERP = "/home/wwagah/repositories/virtualenvs/jewelry-manager-env/bin/python"
 
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
