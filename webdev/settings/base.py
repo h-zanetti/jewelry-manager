ace3ea8e9a3bf0a555662134712231e2ad7dc1db
agah <henriaguiar99@gmail.com>
2021-05-03 18:34:09 -0300

    Consertado leitura de arquivos estáticos em produção close #46

--- webdev/settings/base.py
+++ webdev/settings/base.py
@@ -111,10 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/jewelry/staticfiles/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/jewelry/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
