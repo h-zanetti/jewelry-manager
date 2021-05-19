f759fe92127e84c7e27cdc2c06bbde561a0d0032
agah <henriaguiar99@gmail.com>
2021-05-19 20:16:02 -0300

    Consertado bug de ImproperlyConfigured em importação close #105

--- webdev/settings/production.py
+++ webdev/settings/production.py
@@ -11,7 +11,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['jewelry.agahsolutions.com']
 
