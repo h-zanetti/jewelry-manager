f1b4b1d11d6281bb7bb000bb8e14723249f1a91a
agah <henriaguiar99@gmail.com>
2021-08-05 19:46:12 -0300

    Testada importação de produtos

--- webdev/produtos/tests/test_importacao_exportacao_de_produtos.py
+++ webdev/produtos/tests/test_importacao_exportacao_de_produtos.py
@@ -1,21 +1,13 @@
-from django.utils import timezone
+import os
 import pytest
 from django.urls import reverse
 from django.contrib.auth.models import User
 from pytest_django.asserts import assertContains
 from webdev.produtos.models import Produto
 
+# Botões presentes (GET)
 @pytest.fixture
-def produto(db):
-    return Produto.objects.create(
-        nome='Produto1',
-        colecao="d'Mentira",
-        observacao="Essa é uma observação do produto."
-    )
-
-# GET
-@pytest.fixture
-def resposta_produtos(client, produto):
+def resposta_produtos(client):
     User.objects.create_user(username='TestUser', password='MinhaSenha123')
     client.login(username='TestUser', password='MinhaSenha123')
     resp = client.get(reverse('produtos:estoque_produtos'))
@@ -26,3 +18,22 @@ def test_btn_exportar_produtos_presente(resposta_produtos):
 
 def test_btn_importar_produtos_presente(resposta_produtos):
     assertContains(resposta_produtos, f'href="{reverse("produtos:importar_produtos")}')
+
+# Importação de produtos (POST)
+@pytest.fixture
+def resposta_importar_produtos(client, db):
+    User.objects.create_user(username='TestUser', password='MinhaSenha123')
+    client.login(username='TestUser', password='MinhaSenha123')
+    with open(os.path.join(os.path.dirname(__file__), 'importacoes/produtos.xls'), 'rb') as xl_file:
+        resp = client.post(
+            reverse('produtos:importar_produtos'),
+            data={'myfile': xl_file}
+        )
+        xl_file.close()
+    return resp
+
+def test_status_code(resposta_importar_produtos):
+    assert resposta_importar_produtos.status_code == 302
+
+def test_produto_importado(resposta_importar_produtos):
+    assert Produto.objects.exists()
