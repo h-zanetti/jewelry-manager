faf19240788862cd1c0479614ec6372c0d855e0f
Henrique Zanetti <henriaguiar99@gmail.com>
2022-01-08 13:24:41 -0300

    Finalizadas mudanças de layout

--- webdev/produtos/models.py
+++ webdev/produtos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from webdev.fornecedores.models import Servico
 from webdev.materiais.models import Material
+from django.utils import timezone
 
 class Categoria(models.Model):
     nome = models.CharField(_('Categoria'), max_length=150, unique=True)
